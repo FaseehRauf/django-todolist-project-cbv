957f0a5e65646511321b4b767326e1c0470ab78d
Faseeh Khan <faseeh.khan801@gmail.com>
2026-01-30 23:50:14 -0800

    Added authentication

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
+path('register/', RegisterPage.as_view(), name='user-register' ),
+path('user-login/', CustomLoginView.as_view(), name='user-login' ),  
+path('user-logout/', LogoutView.as_view(next_page='user-login'), name ='user-logout'),
 path('', TaskList.as_view(), name='task-list'),
 path('task-detail/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
 path('task-create/', TaskCreate.as_view(), name='task-create'),
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -111,6 +111,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'user-login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
